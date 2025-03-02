c8eeb8e626af8510936a2ab6f9f28d03b9123dbe
Nakoren <minenkov_vladimir2003@inbox.ru>
2025-03-03 02:32:23 +0300

    Run end state

---
+++ Assets/Scripts/States/States/RunEndState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class RunEndState : IState
+{
+    override protected void OnEnter()
+    {
+    }
+
+    override protected void OnExit()
+    {
+
+    }
+    public void loadNextLevel()
+    {
+        
+    }
+
+    public void toMenu()
+    {
+
+    }
+}
