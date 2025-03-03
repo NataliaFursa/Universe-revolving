89cb1a433d9ffce08484e27d32d1590427d75925
Kaac <Grah228@yandex.ru>
2025-03-03 15:10:43 +0300

    JsonManager add

--- Assets/Scripts/XP and gain/Meta.cs
+++ Assets/Scripts/XP and gain/Meta.cs
@@ -1,4 +1,6 @@
+using System.IO;
 using UnityEngine;
+using static JsonManager;
 
 public static class Meta
 {
@@ -6,4 +8,11 @@ public static class Meta
     public static int baseMS = 8;
     public static float baseHP = 100;
     public static int baseMoney = 150;
+    static public void SetStats(PlayerData playerData)
+    {
+        PlayerXP = playerData.XP;
+        baseMS = playerData.MS;
+        baseHP = playerData.HP;
+        baseMoney = playerData.Money;
+    }
 }
